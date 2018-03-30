b293a9e7f34f3df98be421333c7c3d48c686eae9
hemangi <24651268+HemangiS@users.noreply.github.com>
2018-03-30 16:04:32 +0530

    changes in sh

--- checkJson.sh
+++ checkJson.sh
@@ -5,6 +5,14 @@ JSON_SRC=`git diff HEAD^ HEAD --name-only -- '*.json'`;
 for f in ${JSON_SRC};
 do
   jsonlint $f
+  retval=$?
+  if [[ $retval -eq 0 ]]
+  then
+    echo 'passed'
+  else
+    echo 'failed'
+    exit 1;
+  fi;
 done;
 
 exit 0;
