f0983f5ca6312ddaa9a82358625e93ce800e371d
hemangi <24651268+HemangiS@users.noreply.github.com>
2018-03-30 16:26:16 +0530

    updated sh file and deleted unwanted json file

--- checkJson.sh
+++ checkJson.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-JSON_SRC=`git diff HEAD^ HEAD --name-only -- '*.json'`;
+JSON_SRC=`git diff HEAD^ HEAD --name-only -- '*.json' --diff-filter=ACMRTXB`;
 
 for f in ${JSON_SRC};
 do
-  jsonlint $f
+  jsonlint $f -q
   retval=$?
   if [[ $retval -eq 0 ]]
   then
